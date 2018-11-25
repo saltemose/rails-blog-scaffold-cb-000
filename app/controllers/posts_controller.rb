1ada17024381fedfe02973e9f0ab9148424d4525
Sean Altemose <sean@seanaltemose.com>
2018-11-25 23:27:13 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params(:title))
 
     respond_to do |format|
       if @post.save
